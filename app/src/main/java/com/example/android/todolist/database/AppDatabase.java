cbcba640bea5390dfbebd743d2134aa1d73e6274
Yar <yarsher.dev@gmail.com>
2019-04-23 11:33:10 -0400

    sInstance

--- app/src/main/java/com/example/android/todolist/database/AppDatabase.java
+++ app/src/main/java/com/example/android/todolist/database/AppDatabase.java
@@ -1,6 +1,28 @@
 package com.example.android.todolist.database;
 
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
 
 public abstract class AppDatabase extends RoomDatabase {
+    private static final String TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "todolist";
+
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getInstance(Context context){
+        if (sInstance == null){
+            synchronized (LOCK){
+                Log.d(TAG, "getInstance: Creating new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        Log.d(TAG, "getInstance: Getting the database instance");
+        return sInstance;
+    }
+
+
 }
