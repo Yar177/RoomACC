ac5fd02269eef096e22c064fbcff99875d497959
Yar <yarsher.dev@gmail.com>
2019-04-23 11:36:40 -0400

    Database annotation

--- app/src/main/java/com/example/android/todolist/database/AppDatabase.java
+++ app/src/main/java/com/example/android/todolist/database/AppDatabase.java
@@ -1,10 +1,12 @@
 package com.example.android.todolist.database;
 
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 import android.util.Log;
 
+@Database(entities = {TaskEntry.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static final String TAG = AppDatabase.class.getSimpleName();
     private static final Object LOCK = new Object();
