c8678832aa219acac169c2a50c981e32aa658d2f
Yar <yarsher.dev@gmail.com>
2019-04-23 11:49:17 -0400

    DateConverter

--- app/src/main/java/com/example/android/todolist/database/DateConverter.java
+++ app/src/main/java/com/example/android/todolist/database/DateConverter.java
@@ -1,4 +1,11 @@
 package com.example.android.todolist.database;
 
+import java.util.Date;
+
 public class DateConverter {
+
+    public static Date toDate(Long timestamp){
+        return timestamp == null ? null : new Date(timestamp);
+    }
+
 }
