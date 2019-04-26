31800d0bcb2e5e8c3337ee73ddc386e3607b46d4
Yar <yarsher.dev@gmail.com>
2019-04-26 14:06:28 -0400

    AppExecutors

--- app/src/main/java/com/example/android/todolist/database/AppExecutors.java
+++ app/src/main/java/com/example/android/todolist/database/AppExecutors.java
@@ -1,4 +1,21 @@
 package com.example.android.todolist.database;
 
+import java.util.concurrent.Executor;
+
 public class AppExecutors {
+    //create singlton class
+    private static final Object LOCK = new Object();
+    private static AppExecutors sInstance;
+    private final Executor diskIO;
+    private final Executor mainTHread;
+    private final Executor networkIO;
+
+    public AppExecutors(Executor diskIO, Executor mainTHread, Executor networkIO) {
+        this.diskIO = diskIO;
+        this.mainTHread = mainTHread;
+        this.networkIO = networkIO;
+    }
+
+
+
 }
