074f5ab524c0317a2ff4a2928643c095f4ad294d
Yar <yarsher.dev@gmail.com>
2019-04-29 16:33:32 -0400

    AddTaskViewModel

---
+++ app/src/main/java/com/example/android/todolist/AddTaskViewModel.java
@@ -0,0 +1,9 @@
+package com.example.android.todolist;
+
+import com.example.android.todolist.database.AppDatabase;
+
+class AddTaskViewModel {
+    public AddTaskViewModel(AppDatabase mDB, int mTaskId) {
+
+    }
+}
