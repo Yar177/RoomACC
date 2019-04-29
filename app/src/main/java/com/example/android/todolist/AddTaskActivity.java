88f2a5f9b3502821a69c2eb45a4ef673bbb4fc6a
Yar <yarsher.dev@gmail.com>
2019-04-29 11:39:45 -0400

    LiveData clean up

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.todolist;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -54,17 +57,13 @@ public class AddTaskActivity extends AppCompatActivity {
             if (mTaskId == DEFAULT_TASK_ID) {
                 // populate the UI
                 mTaskId = intent.getIntExtra(EXTRA_TASK_ID, DEFAULT_TASK_ID);
-                AppExecutors.getInstance().diskIO().execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        final TaskEntry taskEntry = mDB.taskDao().loadTaskById(mTaskId);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                populateUI(taskEntry);
-                            }
-                        });
 
+                final LiveData<TaskEntry> task = mDB.taskDao().loadTaskById(mTaskId);
+                task.observe(this, new Observer<TaskEntry>() {
+                    @Override
+                    public void onChanged(@Nullable TaskEntry taskEntry) {
+                        task.removeObserver(this);
+                        populateUI(taskEntry);
                     }
                 });
             }
