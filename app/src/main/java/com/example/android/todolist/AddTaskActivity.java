6bea5d5ad9c6aa7b40abbec919e819f3b964e233
Yar <yarsher.dev@gmail.com>
2019-04-29 11:10:52 -0400

    LiveData

--- app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -1,19 +1,3 @@
-/*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 package com.example.android.todolist;
 
 import android.content.Intent;
@@ -93,9 +77,6 @@ public class AddTaskActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
-    /**
-     * initViews is called from onCreate to init the member variable views
-     */
     private void initViews() {
         mEditText = findViewById(R.id.editTextTaskDescription);
         mRadioGroup = findViewById(R.id.radioGroup);
@@ -109,11 +90,6 @@ public class AddTaskActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * populateUI would be called to populate the UI when in update mode
-     *
-     * @param task the taskEntry to populate the UI
-     */
     private void populateUI(TaskEntry task) {
         if (task == null) return;
 
@@ -122,10 +98,6 @@ public class AddTaskActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * onSaveButtonClicked is called when the "save" button is clicked.
-     * It retrieves user input and inserts that new task data into the underlying database.
-     */
     public void onSaveButtonClicked() {
         // Not yet implemented
         String description = mEditText.getText().toString();
@@ -148,9 +120,7 @@ public class AddTaskActivity extends AppCompatActivity {
 
     }
 
-    /**
-     * getPriority is called whenever the selected priority needs to be retrieved
-     */
+
     public int getPriorityFromViews() {
         int priority = 1;
         int checkedId = ((RadioGroup) findViewById(R.id.radioGroup)).getCheckedRadioButtonId();
@@ -167,11 +137,6 @@ public class AddTaskActivity extends AppCompatActivity {
         return priority;
     }
 
-    /**
-     * setPriority is called when we receive a task from MainActivity
-     *
-     * @param priority the priority value
-     */
     public void setPriorityInViews(int priority) {
         switch (priority) {
             case PRIORITY_HIGH:
--- app/src/main/java/com/example/android/todolist/MainActivity.java
+++ app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -1,18 +1,3 @@
-/*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
 
 package com.example.android.todolist;
 
@@ -68,11 +53,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
         DividerItemDecoration decoration = new DividerItemDecoration(getApplicationContext(), VERTICAL);
         mRecyclerView.addItemDecoration(decoration);
 
-        /*
-         Add a touch helper to the RecyclerView to recognize when a user swipes to delete an item.
-         An ItemTouchHelper enables touch behavior (like swipe and move) on each ViewHolder,
-         and uses callbacks to signal when a user is performing these actions.
-         */
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
             public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
@@ -95,11 +75,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
             }
         }).attachToRecyclerView(mRecyclerView);
 
-        /*
-         Set the Floating Action Button (FAB) to its corresponding View.
-         Attach an OnClickListener to it, so that when it's clicked, a new intent will be created
-         to launch the AddTaskActivity.
-         */
         FloatingActionButton fabButton = findViewById(R.id.fab);
 
         fabButton.setOnClickListener(new View.OnClickListener() {
@@ -114,12 +89,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
         mDB = AppDatabase.getInstance(getApplicationContext());
 
         retriveTasks();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
     }
 
     private void retriveTasks() {
@@ -137,7 +106,6 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.ItemC
 
     @Override
     public void onItemClickListener(int itemId) {
-        // Launch AddTaskActivity adding the itemId as an extra in the intent
         Intent intent = new Intent(MainActivity.this, AddTaskActivity.class);
         intent.putExtra(AddTaskActivity.EXTRA_TASK_ID, itemId);
         startActivity(intent);
