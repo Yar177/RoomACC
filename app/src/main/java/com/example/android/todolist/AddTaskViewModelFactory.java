c91a9ce442526d671c258ac5f4f563e8eba77f8b
Yar <yarsher.dev@gmail.com>
2019-04-29 16:31:08 -0400

    ViewModelFactory

--- app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
+++ app/src/main/java/com/example/android/todolist/AddTaskViewModelFactory.java
@@ -2,7 +2,19 @@ package com.example.android.todolist;
 
 import android.arch.lifecycle.ViewModelProvider;
 
+import com.example.android.todolist.database.AppDatabase;
+
 public class AddTaskViewModelFactory extends ViewModelProvider.NewInstanceFactory {
+
+    private final AppDatabase mDB;
+    private final int mTasjId;
+
+    public AddTaskViewModelFactory(AppDatabase mDB, int mTasjId) {
+        this.mDB = mDB;
+        this.mTasjId = mTasjId;
+    }
+
+
 
 
 }
